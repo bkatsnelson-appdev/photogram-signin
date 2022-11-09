93dbe2ff1b697dfb1ddf786c76e9a85860751a9a
beatricek222 <beatricek222@gmail.com>
2022-11-09 21:25:19 +0000

    Completed Authentication System

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,34 @@
 class UsersController < ApplicationController
+  def authenticate
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+
+    user = User.where({ :username => un })[0]
+
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "No one by that name" })
+    else
+      if user.authenticate(pw)
+        session.store(:user_id, user.id)
+        redirect_to("/", {:notice => "Welcome back #{user.username}!"})
+      else
+        redirect_to("/user_sign_in", {:alert => "Incorrect password, try again."})
+      end
+    end
+  end
+
+  def sign_out
+    reset_session
+
+    redirect_to("/", { :notice => "See ya later!" })
+  end
+
   def new_registration_form
     render({ :template => "users/signup_form.html.erb" })
+  end
+
+  def new_session_form
+    render({ :template => "users/signin_form.html.erb" })
   end
 
   def index
@@ -26,6 +54,7 @@ class UsersController < ApplicationController
     save_status = user.save
 
     if save_status == true
+      session.store(:user_id, user.id)
       redirect_to("/users/#{user.username}", { :notice => "Welcome, #{user.username}!" })
     else
       redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
