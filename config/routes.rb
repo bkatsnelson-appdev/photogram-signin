5f1a1f380556f4ae33f8d414a31eb227c826f578
beatricek222 <beatricek222@gmail.com>
2022-11-09 21:51:40 +0000

    more changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get("/user_sign_in", { :controller => "users", :action => "new_session_form" })
 
   post("verify_credentials", { :controller => "users", :action => "authenticate" })
+
   # CREATE
   get("/insert_user_record", { :controller => "users", :action => "create" })
 
